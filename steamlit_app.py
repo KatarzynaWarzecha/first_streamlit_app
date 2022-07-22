9205357a235c28e067bf5a74521e5abdf9fff4a3
KatarzynaWarzecha <104493623+KatarzynaWarzecha@users.noreply.github.com>
2022-07-22 14:40:46 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,9 +1,1 @@
-import steamlit
-import typing_extensions
-steamlit.title('My parents new healthy dinner')
-steamlit.header('Breakfast Menu')
-steamlit.text('🥣Omega 3 & Bluberries Oatmeal')
-steamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-steamlit.text('🐔Hard-Boiled Free-Range Egg')
-steamlit.text('🥑🍞Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
