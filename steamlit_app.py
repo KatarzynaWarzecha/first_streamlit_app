80342ed97827dfaea9114cc4e7adb96164142842
KatarzynaWarzecha <104493623+KatarzynaWarzecha@users.noreply.github.com>
2022-07-22 13:41:40 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,2 +1,6 @@
 import steamlit
 steamlit.title('My parents new healthy dinner')
+steamlit.header('Breakfast Menu')
+steamlit.text('Omega 3 & Bluberries Oatmeal')
+steamlit.text('Kale, Spinach & Rocket Smoothie')
+steamlit.text('Hard-Boiled Free-Range Egg')
